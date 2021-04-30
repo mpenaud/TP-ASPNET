07ae45dfb5d356fa759d22c26ab3a3d76dcd0009
mpenaud <60395344+mpenaud@users.noreply.github.com>
2021-04-30 09:16:05 +0200

    Add files via upload

---
+++ Pizza - TP2/TP_Pizza/Models/PizzaCreateEditVM.cs
@@ -0,0 +1,19 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP_Pizza.Models
+{
+    public class PizzaCreateEditVM
+    {
+        public Pizza Pizza { get; set; }
+        public List<SelectListItem> Ingredients { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> Pates { get; set; } = new List<SelectListItem>();
+
+        public int IdSelectedPate { get; set; }
+        public List<int> IdSelectedIngredients { get; set; } = new List<int>();
+    }
+}
